e8ca57df54c03b4ab1f4ba429df0fad676f08103
Billy Zhou <billyzs.728@gmail.com>
2016-12-06 21:03:40 -0500

    modified ViewLevel for builder

--- src/builder/ViewMenu.java
+++ src/builder/ViewMenu.java
@@ -1,7 +1,9 @@
 package builder;
 
+import common.model.LightningLevel;
 import common.model.Model;
 import common.model.PuzzleLevel;
+import common.model.ThemeLevel;
 import common.view.IView;
 import common.view.LevelMenuView;
 import common.view.MenuView;
@@ -14,37 +16,12 @@ import java.awt.*;
  */
 public class ViewMenu extends MenuView implements IView{
     //JComboBox boxCreateNewLevel;
-    JButton btnPuzzle;
-    JButton btnLightening;
-    JButton btnTheme;
 
-    public JButton getBtnPuzzle() {
-        return btnPuzzle;
-    }
-
-    public void setBtnPuzzle(JButton btnPuzzle) {
-        this.btnPuzzle = btnPuzzle;
-    }
-
-    public JButton getBtnLightening() {
-        return btnLightening;
-    }
-
-    public void setBtnLightening(JButton btnLightening) {
-        this.btnLightening = btnLightening;
-    }
-
-    public JButton getBtnTheme() {
-        return btnTheme;
-    }
-
-    public void setBtnTheme(JButton btnTheme) {
-        this.btnTheme = btnTheme;
-    }
 
     public ViewMenu(Model m){
-        // PuzzleLevel puzzleLevel = m.
+
         super(m);
+
         //TODO stars, locks, previous high scores shouldn't be needed.
         for(LevelMenuView lmv : this.getLevelMenuViews()){
             lmv.getLabelHighScore().setVisible(false);
@@ -55,7 +32,7 @@ public class ViewMenu extends MenuView implements IView{
         }
 
         int numLevels = m.getLevels().size();
-        setLayout(new GridLayout(numLevels+3, 1, 0, 0)); // +3 because of the 3 new buttons
+        setLayout(new GridLayout(numLevels, 1, 0, 0)); // +3 because of the 3 new buttons
 
 
 
--- src/common/model/Level.java
+++ src/common/model/Level.java
@@ -8,6 +8,38 @@ public abstract class Level{
 	String name;
 	Word currentWord;
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDict(Dictionary dict) {
+		this.dict = dict;
+	}
+
+	public void setCurrentPoints(int currentPoints) {
+		this.currentPoints = currentPoints;
+	}
+
+	public void setHighscore(int highscore) {
+		this.highscore = highscore;
+	}
+
+	public void setStarVals(int[] starVals) {
+		this.starVals = starVals;
+	}
+
+	public boolean isUnlocked() {
+		return unlocked;
+	}
+
+	public void setUnlocked(boolean unlocked) {
+		this.unlocked = unlocked;
+	}
+
+	public void setSubmittedWords(ArrayList<String> submittedWords) {
+		this.submittedWords = submittedWords;
+	}
+
 	//need to establish dictionary implementation
 	Dictionary dict;
 	int currentPoints;
