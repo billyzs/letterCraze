8ebcdb416dffc8f074a67b129f6d69478178a2aa
Grant Espe <gtespe@wpi.edu>
2016-12-12 10:27:43 -0500

    Playable theme levels

--- src/common/model/Board.java
+++ src/common/model/Board.java
@@ -39,8 +39,16 @@ public class Board {
 			}
 		}
     }
-	
-
+    
+    public void clear(){
+		for(ArrayList<Tile> row : this.tiles){
+			for(Tile t : row){
+                t.setLetters("");
+                t.setSelected(false);
+			}
+		}
+    }
+    
 
 	public ArrayList<ArrayList<Tile>> getTiles() {
 		return this.tiles;
@@ -94,5 +102,106 @@ public class Board {
 		}
 		System.out.println(count);
 		return count;
+	}
+
+	/** for theme levels, adds a string to a board
+	 * 
+	 * @param s The string to be added.
+	 * @param remainingTries number of TRials left before algol gives up.
+	 */
+	public void add(String s, int remainingTries) {
+		remainingTries--;
+
+		if(remainingTries <= 0){
+            System.out.println("\n Finished all tries for word: " + s);
+			return;
+		}
+
+		//search and get all empty, nonnull tiles
+		ArrayList<Tile> allEmpty = new ArrayList<Tile>();
+		ArrayList<Tile> used = new ArrayList<Tile>();
+		
+		for(ArrayList<Tile> row : this.tiles){
+			for(Tile t : row){
+				if(!t.isNull() && t.isEmpty())
+                    allEmpty.add(t);
+			}
+		}
+		
+		if(allEmpty.size() < s.length()){
+            System.out.println("\n\nNOt Enough Tiles ON BOARD FOR THEME WORD: " + s);
+			return;
+		}
+		
+		//get a random tile to be first
+		Tile firstTile = allEmpty.get((int)(Math.random()*allEmpty.size()));
+		used.add(firstTile);
+		
+		int row = firstTile.getRow();
+		int col = firstTile.getCol();
+		
+		//set the first letter
+		tiles.get(row).get(col).setLetters("" + s.toCharArray()[0]);
+		
+		//set the rest
+		for(int k = 0; k < s.length(); k++){
+			//Empty adjacent tiles
+            ArrayList<Tile> empties = getEmptyAdjacents(row,col);
+
+            if(empties.size() == 0){
+            	System.err.println("\n\nNO Empty adjacent tiles FOR THEME WORD, trying again: " + s);
+
+            	//reset used tiles to blank and try again
+            	// but if its at the last try, just leave chars.
+            	if(remainingTries > 1)
+                    for(Tile t : used)
+                        t.setLetters("");
+
+            	//try recursively
+            	this.add(s, remainingTries);
+            	return;
+            }
+
+            int rand = (int)(Math.random()*(empties.size()));
+            Tile nextTile = empties.get(rand);
+            used.add(nextTile);
+
+            //set the letter to go there
+            nextTile.setLetters("" + s.toCharArray()[k]);
+
+            //set the rows and col of current letter
+            row = nextTile.getRow();
+            col = nextTile.getCol();
+		}
+
+		System.out.println("Done with word: " + s);
+	}
+
+	public ArrayList<Tile> getEmptyAdjacents(int row, int col) {
+		ArrayList<Tile> result = new ArrayList<Tile>();
+
+		for(int k = -1; k <= 1; k++){
+			for(int i = -1; i <= 1; i++){
+				if(row+k >= 0 && col+i >=0 && row+k < 6 && col+i < 6){
+                    Tile t = this.tiles.get(row + k).get(col + i);
+                    if(!t.isNull() && t.isEmpty())
+                        result.add(t);
+				}
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 * Fills the empty tiles with random letters
+	 */
+	public void fillEmpty() {
+		for(ArrayList<Tile> row : this.tiles){
+			for(Tile t : row){
+				if(t.isEmpty())
+                    t.setRandLetter();
+			}
+		}
 	}
 }
--- src/common/model/ThemeLevel.java
+++ src/common/model/ThemeLevel.java
@@ -1,26 +1,55 @@
 package common.model;
 
+import java.util.ArrayList;
+
 public class ThemeLevel extends Level {
 	public static final String type = "Theme";
 
 	public ThemeLevel(Board b, String n, Dictionary d, int hs, int sv, boolean ul){
 		super(b,n,d,hs,sv,ul);
+		//this.initialize();
 	}
 	
 	protected void repopulate(){
 		//do nothing
 	}
 	
+	/**
+	 * Go through the Dictionary, add all words to the board with algorithm.
+	 */
 	protected void populateThemeWords(){
-		//algorithmically add all words in the dictionary to the board
+		ArrayList<String> strings = new ArrayList<String>(this.dict.getTable());
+		
+		System.out.println(strings);
+
+		//add the strings to the board
+		for(String s : strings){
+			s = s.toUpperCase();
+			//10 is arbitrary number of tries per word
+			this.board.add(s,10);
+		}
+		
 	}
 
 	@Override
-	public void initialize() {
-		// TODO Auto-generated method stub
+	public void initialize(){
+		this.currentWord.initialize();
+
+		//remove all words from submitted words array
+		for(int k = this.submittedWords.size()-1; k >= 0; k--){
+			submittedWords.remove(k);
+		}
 		
+		this.board.clear();
+		this.populateThemeWords();
+		this.populateThemeWords();
+
+		//randomize other letters
+		this.board.fillEmpty();
+
+		this.currentPoints = 0;
 	}
-	
+
 	//because dicitonary needs to be saved to disk
 	protected Dictionary getDictionary(){ return this.dict; }
 
