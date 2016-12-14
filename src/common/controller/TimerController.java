e36a265d23da7d6a6261b793585725c582762e83
santiagosada <ssada96@gmail.com>
2016-12-13 22:52:51 -0800

    Use time amount from actual file

--- src/common/controller/TimerController.java
+++ src/common/controller/TimerController.java
@@ -14,7 +14,7 @@ import common.view.ContentPane;
 import common.view.LevelView;
 
 public class TimerController implements ActionListener{
-    int elapsedSeconds = 30;
+    int targetSeconds;
     JLabel timerLabel;
 	Level level;
 	LevelView levelView;
@@ -23,15 +23,16 @@ public class TimerController implements ActionListener{
 	public TimerController(Level l){
 		this.level= l;
 		this.timer = ((LightningLevel) l).getTimer();
+		targetSeconds = ((LightningLevel)level).getTimerSeconds();
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		this.levelView = ContentPane.getCurrentLevelView();
 		timerLabel = levelView.getTimerLabel();
-		elapsedSeconds--;
-        timerLabel.setText("" + elapsedSeconds);
-        if(elapsedSeconds <= 0){
+		targetSeconds--;
+        timerLabel.setText("" + targetSeconds);
+        if(targetSeconds <= 0){
             timer.stop();
             System.out.println("Timer Over");
         }
