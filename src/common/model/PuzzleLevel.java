646e84c38ccca242f0a88d239c0ed661cfc5f5e4
Grant Espe <gtespe@wpi.edu>
2016-12-15 09:00:06 -0500

    Fixed Score calculation for puzzle levels, added multiplier based on len

--- src/common/model/PuzzleLevel.java
+++ src/common/model/PuzzleLevel.java
@@ -88,6 +88,13 @@ public class PuzzleLevel extends Level{
 		for(Tile t : word.getTiles()){
 			score += this.getLetterScore(t.getLetters());
 		}
+		
+		//multiply score by n-2
+		int multiplier = word.toString().length() - 2;
+		
+		if(multiplier > 1)
+			score *= multiplier;
+			
 		return score;
 	}
 	
