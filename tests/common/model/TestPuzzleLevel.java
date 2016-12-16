6556d20864528c287b4e05816a7e2d740144f17e
AdamCamilli <aocamilli@wpi.edu>
2016-12-15 23:57:06 -0400

    Added tests to TestPuzzleLevel to max out model coverage

--- tests/common/model/TestPuzzleLevel.java
+++ tests/common/model/TestPuzzleLevel.java
@@ -25,5 +25,27 @@ public class TestPuzzleLevel {
 			assertTrue(testLevel2.getMaxWords() == testLevel.getMaxWords());
 		}catch(Exception e){}	
 	}
+	
+	@Test 
+	public void testOther() {
+		try{
+			PuzzleLevel testLevel = (PuzzleLevel)loadLevel("Puzzle_Level_1.lvl");
+			assertEquals(1,testLevel.getLetterScore("E"));
+			assertEquals(2,testLevel.getLetterScore("A"));
+			assertEquals(3,testLevel.getLetterScore("D"));
+			assertEquals(4,testLevel.getLetterScore("G"));
+			assertEquals(5,testLevel.getLetterScore("K"));
+			assertEquals(7,testLevel.getLetterScore("J"));
+			assertEquals(8,testLevel.getLetterScore("Z"));
+			assertEquals(11,testLevel.getLetterScore("Q"));
+			
+			Tile t = new Tile("A",0,0);
+			Word w = new Word();
+			w.addTile(t);
+			assertEquals(2,testLevel.computeScore(w));
+		} catch (Exception e) {
+			fail();
+		}
+	}
 
 }
